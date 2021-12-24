331362a6cad2fcc01b8c4490640c48c273aa6d46
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-12-24 11:11:36 +0530

    Add files via upload

---
+++ RealTimeChatWebApp/src/main/java/com/durgesh/RealmeTimeChatWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.durgesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealmeTimeChatWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealmeTimeChatWebAppApplication.class, args);
+	}
+
+}
