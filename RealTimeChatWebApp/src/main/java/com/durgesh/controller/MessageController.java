74c456971a3d0325bc10632b8d9491e4105be21f
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-12-24 11:49:52 +0530

    Update MessageController.java

--- RealTimeChatWebApp/src/main/java/com/durgesh/controller/MessageController.java
+++ RealTimeChatWebApp/src/main/java/com/durgesh/controller/MessageController.java
@@ -13,14 +13,6 @@ public class MessageController {
     @MessageMapping("/message")
     @SendTo("/topic/return-to")
     public Message getContent(@RequestBody Message message) {
-
-//        try {
-//            //processing
-////            Thread.sleep(2000);
-//
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
         return message;
     }
 
